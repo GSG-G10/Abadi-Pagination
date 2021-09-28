3818ddd39be0dea00be3caf871484a77ea4de9e2
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-09-28 23:36:55 +0300

    final Design

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import './App.css';
+import { Pagination, Card, Row, Col, Slider } from 'antd';
+import React, { useEffect, useState } from 'react';
+import 'antd/dist/antd.css';
+
+function App() {
+  const { Meta } = Card;
+
+  const [number, setNumber] = useState(1);
+
+  const [dataa, setDataa] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.unsplash.com/search/collections?page=${number}&limit=10&query=cat&client_id=V0G0X78-1-W7P0OS0w1Y7bift63SE2YXdP1bYH7jT30`
+    )
+      .then((res) => res.json())
+      .then((data) => setDataa(data.results));
+  }, [number]);
+
+  const onchangee = (page, pageSize) => {
+    setNumber(page);
+  };
+
+  console.log(number);
+  console.log(dataa);
+  // ele.cover_photo.urls.small
+  return (
+    <div className="App">
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: '20px',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+        }}
+      >
+        {dataa.length !== 0
+          ? dataa.map((ele) => {
+              console.log('aaaaaaaaaaaaaaaaaa', ele.tags[0].source);
+              return (
+                <Card
+                  hoverable
+                  style={{ width: 240, margin: '20px', flex: '2' }}
+                  cover={<img alt="example" src={ele.cover_photo.urls.small} />}
+                >
+                  <Meta
+                    title={
+                      ele.tags[0].source !== undefined
+                        ? ele.tags[0].source.title
+                        : 'Null'
+                    }
+                    description={
+                      ele.tags[0].source !== undefined
+                        ? ele.tags[0].source.description
+                        : 'Null'
+                    }
+                  />
+                </Card>
+              );
+            })
+          : 'lodaing ...'}
+      </div>
+      ,
+      <div>
+        <Pagination onChange={onchangee} defaultCurrent={1} total={50} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
